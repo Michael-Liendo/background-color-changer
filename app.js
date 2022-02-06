460561ab9fc408f255ca3f4e8be001261bc28546
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-02-06 17:32:38 -0400

    feat(initial background)

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let button = document.querySelector("button");
 
 button.addEventListener("click", backgroundChangerColor);
 
+let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+document.body.style.background = `#${randomColor}`;
+
 function backgroundChangerColor() {
   let randomColor = Math.floor(Math.random() * 16777215).toString(16);
   console.log(`#${randomColor}`);
